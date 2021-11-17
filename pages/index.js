7db3172c623d2490cff4da16b9d2064bc9963a3e
BhargavKadali39 <bhargavkadali39@gmail.com>
2021-11-17 14:33:45 +0530

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,12 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  let arr = [1, 2, 3];
-  for (let i = 0; i < 3; i++) {
-    return (
-      <div>
-        hello world <p> {i} </p>{' '}
-      </div>
-    );
-  }
+  let arr = ['this', 'is', 'me'];
+  return (
+    <div>
+      {arr.map((text) => (
+        <p>{text}</p>
+      ))}
+    </div>
+  );
 }
