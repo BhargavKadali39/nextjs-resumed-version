a6b2153afa89b5f4b3afd85dbab7f7bc47af3ee2
BhargavKadali39 <bhargavkadali39@gmail.com>
2021-11-17 16:03:51 +0530

    1 file modified, 1 added

---
+++ components/Limp.js
@@ -0,0 +1,9 @@
+export default function NameUs({a,b,c}){
+  return(
+    <ul>
+      <li>{a}</li>
+      <li>{b}</li>
+      <li>{c}</li>
+    </ul>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,11 @@
- import Head from 'next/head';
+import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Field from '../components/Limp';
 
 export default function Home() {
-  let arr = ['this', 'is', 'me'];
   return (
     <div>
-      {arr.map((text) => (
-        <p>{text}</p>
-      ))}
+      <Field a="1" b="2" c="3" />
     </div>
   );
 }
