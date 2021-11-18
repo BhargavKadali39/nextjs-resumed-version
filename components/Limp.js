b8da17b180b076f792964bd0d25117f41ea5d328
BhargavKadali39 <bhargavkadali39@gmail.com>
2021-11-18 23:14:23 +0530

    2 files modified

--- components/Limp.js
+++ components/Limp.js
@@ -4,7 +4,17 @@ export default function NameUs({ a, b, c }) {
       <li>{a}</li>
       <li>{b}</li>
       <li>{c}</li>
-      <button onclick="setClipboard('{a}'">copy</button>
+      <button onclick="setCl('{a}')">copy</button>
     </ul>
   );
+  function setCl(value) {
+    var tempInput = document.createElement('input');
+    tempInput.style = 'position: absolute; left: -1000px; top: -1000px';
+    tempInput.value = value;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand('copy');
+    document.body.removeChild(tempInput);
+    alert('Copied');
+  }
 }
