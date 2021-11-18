5b65cb992ec62989ddbba247bb6097ec6d00ed9b
BhargavKadali39 <bhargavkadali39@gmail.com>
2021-11-18 23:11:51 +0530

    2 files modified

--- components/Limp.js
+++ components/Limp.js
@@ -1,9 +1,10 @@
-export default function NameUs({a,b,c}){
-  return(
+export default function NameUs({ a, b, c }) {
+  return (
     <ul>
       <li>{a}</li>
       <li>{b}</li>
       <li>{c}</li>
+      <button onclick="setClipboard('{a}'">copy</button>
     </ul>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,10 @@ export default function Home() {
       <Field a="1" b="2" c="3" />
       <Field a="4" b="5" c="6" />
       <Field a="7" b="8" c="9" />
+      <Field a="background: rgb(1,19,55);" b="8" c="9" />
+      <div class="g10">
+        <button onclick="setClipboard(''">copy</button>
+      </div>
     </div>
   );
 }
