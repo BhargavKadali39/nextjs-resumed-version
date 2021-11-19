bad7b2fb8bfc79f25b6255858e9e17c99a9cce6a
BhargavKadali39 <bhargavkadali39@gmail.com>
2021-11-19 23:28:33 +0530

    1 file modified

--- components/Limp.js
+++ components/Limp.js
@@ -1,16 +1,16 @@
 export default function NameUs({ a, b, c }) {
-  function setCl(value) {
-    if (process.browser) {
-      var tempInput = document.createElement('input');
-      tempInput.style = 'position: absolute; left: -1000px; top: -1000px';
-      tempInput.value = value;
-      document.body.appendChild(tempInput);
-      tempInput.select();
-      document.execCommand('copy');
-      document.body.removeChild(tempInput);
-      alert('Copied');
-    }
-  }
+  // function setCl(value) {
+  //   if (process.browser) {
+  //     var tempInput = document.createElement('input');
+  //     tempInput.style = 'position: absolute; left: -1000px; top: -1000px';
+  //     tempInput.value = value;
+  //     document.body.appendChild(tempInput);
+  //     tempInput.select();
+  //     document.execCommand('copy');
+  //     document.body.removeChild(tempInput);
+  //     alert('Copied');
+  //   }
+  // }
   return (
     <ul>
       <li>{a}</li>
